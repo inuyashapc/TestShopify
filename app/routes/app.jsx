ba1e33390d2cb2fc731623286509f4d980574523
Duy Anh Dove <inuyashapc162@gmail.com>
2025-04-29 10:23:21 +0700

    feat: product info toast message liquid

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -22,7 +22,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/qr-list">QR List</Link>
         <Link to="/app/product">List Product</Link>
       </NavMenu>
       <Outlet />
